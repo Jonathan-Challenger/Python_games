112e35023fad27be1afcbe8acc1de3c07acea25b
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2024-01-11 11:42:42 +0000

    first try

--- Hangman Game/maingame.py
+++ Hangman Game/maingame.py
@@ -1,1 +1,16 @@
+import random
+import tkinter
+
+def get_word():
+    wordlist = []
+    with open("hangman_wordlist.txt", "r") as file:
+        wordlist = file.read().split("\n")
+
+    word = random.choice(wordlist)
+    return word
+
+get_word()
+
+
+    
 
